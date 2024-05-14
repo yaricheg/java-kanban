5f7cfbd741a1fa98b964b278f11f88bc3282342a
нфÐyarichekp <yanormik@gmail.com>
2024-05-14 13:48:55 +0300

    Проект по 6 тз исправления в CheckStyle

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -3,6 +3,7 @@ public class Managers {
     public static TaskManager getDefaults() {
         return new InMemoryTaskManager(getDefaultHistory());
     }
+
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
